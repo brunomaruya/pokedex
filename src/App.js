3cbf6bd12f187bfeef68868922a32a959632ccb1
bruno maruya <brun0yuj1ym02@gmail.com>
2024-10-03 15:39:51 +0900

    chore: add logo image to assets/images

--- src/App.js
+++ src/App.js
@@ -23,11 +23,7 @@ function App() {
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Error</div>;
-  return (
-    <div>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
