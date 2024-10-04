4bc06fb0559ef62e86d40230e54a6b6384039c50
bruno maruya <brun0yuj1ym02@gmail.com>
2024-10-04 13:07:31 +0900

    feat:update App and add PokemonProfile

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import "./index.css";
 import "./App.css";
 import { Route, Router, Routes } from "react-router-dom";
 import Home from "./page/Home";
+import PokemonProfile from "./page/PokemonProfile";
 
 function App() {
   <Router>
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/pokemon/:id" element={<PokemonProfile />} />
     </Routes>
   </Router>;
 }
