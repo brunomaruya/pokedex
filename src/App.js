7549908625c4b9b66200a63be49e05debb2c1d72
bruno maruya <brun0yuj1ym02@gmail.com>
2024-10-04 13:15:41 +0900

    feat: add dynamic page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import "./index.css";
 import "./App.css";
-import { Route, Router, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./page/Home";
 import PokemonProfile from "./page/PokemonProfile";
 
 function App() {
-  <Router>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/pokemon/:id" element={<PokemonProfile />} />
-    </Routes>
-  </Router>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/pokemon/:id" element={<PokemonProfile />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/page/PokemonProfile.jsx
+++ src/page/PokemonProfile.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 
 export default function PokemonProfile() {
-  return <div>PokemonProfile</div>;
+  const { id } = useParams();
+  return <div>PokemonProfile: {id}</div>;
 }
