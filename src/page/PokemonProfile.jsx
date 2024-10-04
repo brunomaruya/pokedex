72a7c0a4774d98cb0b5290c69692ea2392451c67
bruno maruya <brun0yuj1ym02@gmail.com>
2024-10-04 21:37:51 +0900

    feat: get data to dynamica page

--- src/page/PokemonProfile.jsx
+++ src/page/PokemonProfile.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function PokemonProfile() {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   const { id } = useParams();
+
+  useEffect(() => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+      .then((response) => {
+        console.log(response.data);
+        setData(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+  if (loading) return <div>Loading...</div>;
+
+  if (error) return <div>Error</div>;
+
   return <div>PokemonProfile: {id}</div>;
 }
