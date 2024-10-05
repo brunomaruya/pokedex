a101274e46d873004cb47190ee14bf4ae2bd6b71
bruno maruya <brun0yuj1ym02@gmail.com>
2024-10-06 00:49:21 +0900

    style: add responsive design to PokemonProfile

--- src/page/PokemonProfile.jsx
+++ src/page/PokemonProfile.jsx
@@ -53,69 +53,71 @@ export default function PokemonProfile() {
   if (error) return <div>Error</div>;
 
   return (
-    <main className="mb-20">
-      <section className="container flex flex-col p-5 bg-white rounded-lg shadow-custom-xl">
+    <main className="container max-w-[1024px] mx-auto mb-20">
+      <section className="container flex flex-col md:flex-row md:justify-around p-5 bg-white rounded-lg shadow-custom-xl">
         {/* IMAGE */}
         <img
           src={pokemon.sprites.other["official-artwork"].front_default}
           alt={pokemon.name}
         />
         {/* HEADER */}
-        <div className="flex justify-between w-full ">
-          <div>{idFormatter(pokemon.id)}</div>
-          <div>
-            <SpeakerWaveIcon className="w-6 h-6" />
-          </div>
-        </div>
-        <h1 className="text-3xl font-bold">
-          {capitalizeFirstLetter(pokemon.name)}
-        </h1>
-        {/* TYPES  */}
-        <div className="float-start flex gap-1">
-          {pokemon.types.map((type) => {
-            return (
-              <div
-                key={type.type.name}
-                className="py-1 px-3 text-xs text-white bg-buttonRed  rounded-full"
-              >
-                {type.type.name}
-              </div>
-            );
-          })}
-        </div>
-        {/* DESCRIPTION  */}
         <div>
-          {species?.flavor_text_entries
-            .find((entry) => entry.language.name === "en")
-            .flavor_text.replace(/\f/g, "")}
-        </div>
-        <div className="flex gap-2">
-          <InfoCard label="Height" value={pokemon.height / 10 + "m"} />
-          <InfoCard label="Weight" value={pokemon.weight / 10 + "kg"} />
-        </div>
-        {/* STATS  */}
-        <h2 className="text-2xl font-bold">Stats</h2>
-        <div className="grid grid-cols-2 gap-2">
-          {pokemon.stats.map((stat) => (
-            <InfoCard
-              label={capitalizeFirstLetter(stat.stat.name)}
-              value={stat.base_stat}
-            />
-          ))}
-        </div>
-        {/* ABILITIES  */}
-        <h2 className="text-2xl font-bold">Abilities</h2>
-        <div className="grid grid-cols-2 gap-2">
-          {pokemon.abilities.map((ability) => (
-            <InfoCard label={capitalizeFirstLetter(ability.ability.name)} />
-          ))}
+          <div className="flex justify-between w-full ">
+            <div>{idFormatter(pokemon.id)}</div>
+            <div>
+              <SpeakerWaveIcon className="w-6 h-6" />
+            </div>
+          </div>
+          <h1 className="text-3xl font-bold">
+            {capitalizeFirstLetter(pokemon.name)}
+          </h1>
+          {/* TYPES  */}
+          <div className="float-start flex gap-1">
+            {pokemon.types.map((type) => {
+              return (
+                <div
+                  key={type.type.name}
+                  className="py-1 px-3 text-xs text-white bg-buttonRed  rounded-full"
+                >
+                  {type.type.name}
+                </div>
+              );
+            })}
+          </div>
+          {/* DESCRIPTION  */}
+          <div>
+            {species?.flavor_text_entries
+              .find((entry) => entry.language.name === "en")
+              .flavor_text.replace(/\f/g, "")}
+          </div>
+          <div className="flex gap-2">
+            <InfoCard label="Height" value={pokemon.height / 10 + "m"} />
+            <InfoCard label="Weight" value={pokemon.weight / 10 + "kg"} />
+          </div>
+          {/* STATS  */}
+          <h2 className="text-2xl font-bold">Stats</h2>
+          <div className="grid grid-cols-2 gap-2">
+            {pokemon.stats.map((stat) => (
+              <InfoCard
+                label={capitalizeFirstLetter(stat.stat.name)}
+                value={stat.base_stat}
+              />
+            ))}
+          </div>
+          {/* ABILITIES  */}
+          <h2 className="text-2xl font-bold">Abilities</h2>
+          <div className="grid grid-cols-2 gap-2">
+            {pokemon.abilities.map((ability) => (
+              <InfoCard label={capitalizeFirstLetter(ability.ability.name)} />
+            ))}
+          </div>
         </div>
       </section>
       {/* EVOLUTION CHAIN  */}
       <section>
-        <h2 className="text-2xl font-bold text-white">Evolution </h2>
+        <h2 className="text-2xl font-bold text-white ml-5">Evolution </h2>
 
-        <div className="flex justify-between items-center">
+        <div className="flex justify-around items-center">
           {evolutionChain.map((pokemon, index) => (
             <React.Fragment key={pokemon.id}>
               <div className="flex flex-col items-center">
@@ -128,7 +130,7 @@ export default function PokemonProfile() {
                     alt={pokemon.name}
                   />
                 </Link>
-                <span className="text-sm text-white">{pokemon.name}</span>
+                <div className="text-sm text-white">{pokemon.name}</div>
               </div>
               {/* Renderize a seta apenas se não for o último Pokémon */}
               {index < evolutionChain.length - 1 && (
