a9e8276707e188b5b5c4f54467c6d1a665c1ec96
bruno maruya <brun0yuj1ym02@gmail.com>
2024-10-07 18:58:40 +0900

    feat: add SearchProvider at App

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./page/Home";
 import PokemonProfile from "./page/PokemonProfile";
 import Header from "./components/Header";
+import { SearchProvider } from "./context/SearchContext";
 function App() {
   return (
-    <Router>
-      <main className="m-5">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/pokemon/:id" element={<PokemonProfile />} />
-        </Routes>
-      </main>
-    </Router>
+    <SearchProvider>
+      <Router>
+        <main className="m-5">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/pokemon/:id" element={<PokemonProfile />} />
+          </Routes>
+        </main>
+      </Router>
+    </SearchProvider>
   );
 }
 
