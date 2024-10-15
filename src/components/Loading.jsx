74245424acfb6e26523366bac7a76bc82c3b43f4
bruno maruya <brun0yuj1ym02@gmail.com>
2024-10-15 14:17:27 +0900

    fix: change class to className at Loading component

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ import pokeball from "../images/pokeball.png";
 
 export default function Loading() {
   return (
-    <div class="absolute inset-0 flex items-center justify-center">
+    <div className="absolute inset-0 flex items-center justify-center">
       <img
         src={pokeball}
         className="rotate-infinite w-24 h-24 shadow-custom-xl rounded-full"
