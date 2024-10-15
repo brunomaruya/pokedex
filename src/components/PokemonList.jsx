655c860e79d4036668981654eef25f76c912da6b
bruno maruya <brun0yuj1ym02@gmail.com>
2024-10-15 14:15:22 +0900

    feat: add No results div at PokemonList componentt

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -6,10 +6,13 @@ export default function PokemonList(pokemons) {
 
   return (
     <section className="mt-10 flex flex-col gap-3 w-full sm:grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-      {pokemons &&
+      {pokemons.data.length > 0 ? (
         pokemons.data.map((pokemon) => (
           <PokemonCard key={pokemon.name} pokemons={pokemon.name} />
-        ))}
+        ))
+      ) : (
+        <div className=" text-white">No results</div>
+      )}
     </section>
   );
 }
