62c03457208e387842f95701c083be8b52cfa7d2
bruno maruya <brun0yuj1ym02@gmail.com>
2024-10-18 11:59:49 +0900

    feat: add Evoution Details

--- src/components/EvolutionStage.jsx
+++ src/components/EvolutionStage.jsx
@@ -1,60 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+function EvolutionDetails(pokemon) {
+  console.log(pokemon);
+  return (
+    <div>
+      <div className="flex flex-col items-center">
+        <Link to={`/pokemon/${pokemon.pokemon.id}`}>
+          {pokemon.pokemon.sprites.other["official-artwork"].front_default ? (
+            <img
+              className="w-20 h-20"
+              src={
+                pokemon.pokemon.sprites.other["official-artwork"].front_default
+              }
+              alt={pokemon.pokemon.name}
+            />
+          ) : (
+            <img
+              src={pokemon.pokemon.sprites.front_default}
+              alt={pokemon.pokemon.name}
+              className="w-full h-auto"
+            />
+          )}
+        </Link>
+        <div className="text-sm text-white">{pokemon.pokemon.name}</div>
+      </div>
+    </div>
+  );
+}
+
 export default function EvolutionStage({ pokemon }) {
   return (
     <>
       {pokemon.length > 1 ? (
         <span className="mx-2 text-2xl">
           {pokemon.map((pokemon) => {
-            return (
-              <div className="flex flex-col items-center">
-                <Link to={`/pokemon/${pokemon.pokemon.id}`}>
-                  {pokemon.pokemon.sprites.other["official-artwork"]
-                    .front_default ? (
-                    <img
-                      className="w-20 h-20"
-                      src={
-                        pokemon.pokemon.sprites.other["official-artwork"]
-                          .front_default
-                      }
-                      alt={pokemon.pokemon.name}
-                    />
-                  ) : (
-                    <img
-                      src={pokemon.pokemon.sprites.front_default}
-                      alt={pokemon.pokemon.name}
-                      className="w-full h-auto"
-                    />
-                  )}
-                </Link>
-                <div className="text-sm text-white">{pokemon.pokemon.name}</div>
-              </div>
-            );
+            return EvolutionDetails(pokemon);
           })}
         </span>
       ) : (
-        <div className="flex flex-col items-center">
-          <Link to={`/pokemon/${pokemon.pokemon.id}`}>
-            {pokemon.pokemon.sprites.other["official-artwork"].front_default ? (
-              <img
-                className="w-20 h-20"
-                src={
-                  pokemon.pokemon.sprites.other["official-artwork"]
-                    .front_default
-                }
-                alt={pokemon.pokemon.name}
-              />
-            ) : (
-              <img
-                src={pokemon.pokemon.sprites.front_default}
-                alt={pokemon.pokemon.name}
-                className="w-full h-auto"
-              />
-            )}
-          </Link>
-          <div className="text-sm text-white">{pokemon.pokemon.name}</div>
-        </div>
+        EvolutionDetails(pokemon)
       )}
     </>
   );
